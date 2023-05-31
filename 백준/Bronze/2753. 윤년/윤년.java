fb0fa213e1d104c05e0737f4928b1f1fc258344d
holymoly <121756729+werybalert@users.noreply.github.com>
2023-05-31 23:08:32 +0900

    [Bronze V] Title: 윤년, Time: 212 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main{
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a;
+        a = sc.nextInt();
+ 
+	sc.close();      
+
+	if (a%4 == 0) {		
+		if (a%400 ==0) 
+			System.out.println("1");
+		else if (a%100 ==0)		
+			System.out.println("0");
+		else 	
+			System.out.println("1");
+				}
+		else 	
+			System.out.println("0");
+    				} 
+}
