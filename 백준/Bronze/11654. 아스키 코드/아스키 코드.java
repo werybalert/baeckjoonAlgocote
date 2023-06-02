9dde7956f585d695938191ccab799289a51554f0
holymoly <121756729+werybalert@users.noreply.github.com>
2023-06-02 20:15:01 +0900

    [Bronze V] Title: 아스키 코드, Time: 200 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int As = sc.next().charAt(0);
+        System.out.print(As);
+
+    }
+}
