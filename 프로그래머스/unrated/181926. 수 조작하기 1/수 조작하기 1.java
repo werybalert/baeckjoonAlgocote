1ca23567921370e719af84c0eb08e74669a092b4
holymoly <121756729+werybalert@users.noreply.github.com>
2023-06-10 15:50:07 +0900

    [unrated] Title: 수 조작하기 1, Time: 1.01 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.java
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.java
@@ -1,20 +1,24 @@
 class Solution {
     public int solution(int n, String control) {
         int answer = n;
-        String[] controlArray = control.split("");
-
-        for (int i = 0; i < controlArray.length; i++) {
-            if (controlArray[i].equals("w")) {
-                answer += 1;
-            } else if (controlArray[i].equals("s")) {
-                answer -= 1;
-            } else if (controlArray[i].equals("d")) {
-                answer += 10;
-            } else if (controlArray[i].equals("a")) {
-                answer -= 10;
+        
+        for (char c : control.toCharArray()) {
+            switch(c) {
+                case 'w':
+                    n++;
+                    break;
+                case 's' :
+                    n--;
+                    break;
+                case 'd' :
+                    n +=10;
+                    break;
+                case 'a' :
+                    n -=10;
+                    break;
+                
             }
         }
-
-        return answer;
+        return n;
     }
 }
