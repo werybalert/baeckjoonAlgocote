f796bb93b96fee25ec94d128e972d0f78e3e3468
holymoly <121756729+werybalert@users.noreply.github.com>
2023-07-14 19:47:06 +0900

    [Bronze II] Title: 거스름돈, Time: 124 ms, Memory: 14112 KB -BaekjoonHub

---
+++ 백준/Bronze/5585. 거스름돈/거스름돈.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int cost = Integer.parseInt(br.readLine());
+        int[] coinArr = {500, 100, 50, 10, 5, 1};
+        cost = 1000 - cost;
+
+
+        int num =0;
+        for(int i=0; i<6; i++){
+            if(cost/coinArr[i]>0){
+                num += cost/coinArr[i];
+                cost = cost%coinArr[i];
+            }
+        }
+        System.out.println(num);
+    }
+
+}
