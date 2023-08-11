ca37e271c7a081c09277ad68985e010eeb528dac
holymoly <121756729+werybalert@users.noreply.github.com>
2023-08-12 01:21:23 +0900

    [level 0] Title: 로그인 성공?, Time: 0.03 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+
+class Solution {
+    public String solution(String[] id_pw, String[][] db) {
+       Map<String, String> dbMap = new HashMap<String, String>();
+
+        for (String[] strings : db) {
+            dbMap.put(strings[0], strings[1]);
+        }
+
+        if (dbMap.containsKey(id_pw[0])) {
+            if (dbMap.get(id_pw[0]).equals(id_pw[1])) {
+                return "login";
+            } else {
+                return "wrong pw";
+            }
+        } else {
+            return "fail";
+        }
+    }
+}
